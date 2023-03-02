f34308264b40a81a299708ffc383c05dee8bdc4a
AmitS972801~ <amitsacco77@gmail.com>
2023-03-02 09:48:41 +0100

    x<a

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCR7rSzDjIl1-tTB1ssIIjdmzS8g0YYqnU',
         {
           email: email,
           password: password,
@@ -47,7 +47,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCR7rSzDjIl1-tTB1ssIIjdmzS8g0YYqnU',
         {
           email: email,
           password: password,
