4adab1191c32e02658669d8ca40520d33e64d1d8
AmitS972801~ <amitsacco77@gmail.com>
2023-05-12 14:17:06 +0200

    2f

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,16 @@
+import { 
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot
+} from "@angular/router";
+
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({ provideIn: 'root' })
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    router: RouterStateSnapshot
+  ): 
+}
